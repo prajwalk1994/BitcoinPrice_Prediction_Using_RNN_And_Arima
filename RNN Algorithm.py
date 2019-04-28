8324e7db274ae2395eb5e5730a5bc3e147c815da
Aishwarya Sharma <ashfranky@gmail.com>
2019-04-27 20:10:47 -0700

    Revert "Merge branch 'master' of https://github.com/prajwalk1994/BitcoinPrice_Prediction_Using_RNN_And_Arima"
    This reverts commit 9406323e81f1f83e7e0ffeaaa5156876b9c2d222, reversing
    changes made to f5183a99143128c0ec39f16c700f48a6fd566863.

--- RNN Algorithm.py
+++ RNN Algorithm.py
@@ -6,7 +6,7 @@ from statsmodels.tsa.arima_model import ARIMA
 from pandas import DataFrame
 from matplotlib import pyplot
 #from pandas.tools.plotting import autocorrelation_plot
-df = pd.read_csv('bitstamp.csv')
+df = pd.read_csv('C:\Users\Aishwariya\Desktop\CMPE256 Project\test.csv')
 df
 
 df.fillna(method='ffill', inplace=True)
@@ -17,7 +17,7 @@ df
 def parser(x):
 	return pd.to_datetime(x, unit='s')
 
-series = read_csv('bitstamp.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
+series = read_csv('C:\\Users\\Aishwariya\\Desktop\\CMPE256 Project\\test.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
 series.fillna(method='ffill', inplace=True)
 series
 #autocorrelation_plot(series)
