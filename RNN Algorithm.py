cc5f4ad9d32968e720814674ea226e5e645df176
AishwaryaSharma27 <ashfranky@gmail.com>
2019-04-30 14:32:33 -0700

    Train and test data segregation

--- RNN Algorithm.py
+++ RNN Algorithm.py
@@ -63,3 +63,10 @@ regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
 # Fitting the RNN to the Training set
 regressor.fit(X_train, y_train, batch_size = 5, epochs = 100)
+
+test_set = df_test.values
+inputs = np.reshape(test_set, (len(test_set), 1))
+inputs = sc.transform(inputs)
+inputs = np.reshape(inputs, (len(inputs), 1, 1))
+predicted_BTC_price = regressor.predict(inputs)
+predicted_BTC_price = sc.inverse_transform(predicted_BTC_price)
